20efbcc53edfeac2e4b3c2b7915620c893381abc
Lucas César Felicíssimo <lucas0697@gmail.com>
2014-12-02 22:51:07 -0200

    Revert "Revert "Array opt""
    This reverts commit f80043fdf1eededb13ca89e86d4a57a3f439ab32.

---
+++ questoes.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+  <title> Teste 01 </title>
+  <meta charset = 'UTF-8' />
+  <base href="<?php echo URL; ?>">
+  <script type="text/javascript" src="js/js.js">
+  </script>
+</head> 
+
+<body >
+  <!-- Aparece depois de selecionar materia no SELECT -->
+  <h1 id = "subject"> Selecione a matéria:  </h1>
+  <form method='POST' action="questoes/add">
+    <table id = "table">
+     <tr> 
+      <td id="select_subject">
+       <select id = "subject_opt" name="subject_opt" onchange="changeTitle(this.value), addTag(this.value)">
+        <option> Materia 01 </option>
+        <option> Materia 02 </option>
+        <option value="add" > Adicionar </option>
+        <!-- se for adicionar nova materia, aparece um prompt ou input ao lado -->
+      </select>
+
+    </td>       
+  </tr>
+
+  <tr> 
+    <td> Tipo: </td>
+    <td><input type = "radio" name = "type" id="type" value = "1" onclick="setType(this.value)"> Aberta</td>
+    <td><input type = "radio" name = "type" id="type" value = "2" onclick="setType(this.value)"> Multipla Escolha</td>
+    <td><input type = "radio" name = "type" id="type" value = "3" onclick="setType(this.value)"> Verdadeiro/Falso</td>
+  </tr>
+  <tr> 
+    <td> Tags: </td>
+    <td colspan = "3"> <input type = "text" id = "tags" name="tags" size = "50"> </td>
+
+  </tr>
+  <tr> 
+    <td> Dificuldade: </td>
+    <td><input type = "radio" name = "difficulty" id="difficulty" value = "1" > Fácil</td>
+    <td><input type = "radio" name = "difficulty" id="difficulty" value = "2" > Médio</td>
+    <td><input type = "radio" name = "difficulty" id="difficulty" value = "3" > Difícil</td>
+  </tr>
+  <tr> 
+    <td> Enunciado: </td>
+    <td colspan = "3"> <input type = "text" id = "question" name="question" size = "50"></td>
+  </tr>
+  <tr>
+    <td>
+     Opções: 
+   </td>
+   <td> <input type = "button" id = "add" value = "+" onclick= "createOptions()"></td>
+   <td>
+   </td>
+   <td>
+   </td>
+ </tr>
+</table>
+<input type = "reset" value = "Limpar" >
+<input type = "submit" value = "Enviar" >
+</form>
+</body>
+
+</html>
