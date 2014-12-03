f80043fdf1eededb13ca89e86d4a57a3f439ab32
Lucas César Felicíssimo <lucas0697@gmail.com>
2014-12-02 22:50:46 -0200

    Revert "Array opt"
    This reverts commit ac1ef6cb92b15706140487f94a45a001e70fda81.

--- questoesController.class.php
+++ questoesController.class.php
@@ -9,45 +9,23 @@ class questoes extends Controller
 	function __construct(){}
 
 	public function home(){
-		$this->view('questoes');
+		$this->view('Teste01');
 	}
 
 	public function add(){
-		echo "<base href='<?php echo URL; ?>'";
-		$subject = $_POST["subject_opt"];
-		$type = $_POST["type"];
-		$tags = $_POST["tags"];
-		$difficulty = $_POST['difficulty'];
-		$question = $_POST["question"];
-		$numero_opcoes = "<script src='js/js.js'></script><script>document.write(i)</script>";
-		$i = 1;
-		
-		/*$opcao = $_POST["opt1"];
-		$opcao2 = $_POST["opt2"];
-		echo ">>>" . $numero_opcoes . $subject . "<br/>" . $opcao . "  " . $opcao2;*/
-
-		while ( !empty($_POST["opt" . $i]) ) {
-			$opcoes[ ($i - 1) ] = $_POST["opt" . $i];
-			$i++;	
-		}
-
-		var_dump($opcoes);
-
-/*
-		if ($type == 1) {
-			for ($i=0; $i <  ; $i++) { 
-				$opts[] = 
-			}
-		}
-
+		$tipo = $_POST["tipo"];
+		$dificuldade = $_POST['dificuldade'];
+		$enunciado = $_POST["enunciado"];
+		$materia = $_POST["materia"];
 		$numero_opcoes = $_POST["numero"];
 		$resposta = $_POST["resposta"];
+		$assunto = $_POST["assunto"];
 		for ($i=0; $i < $numero_opcoes ; $i++) { 
 			$opcoes[$i] = $_POST["opcao" . $i];
 		}
 
 		$questao = new Questao();
-		$questao->add($enunciado, $tipo, $materia, $dificuldade, $resposta, $opcoes);*/
+		$questao->add($enunciado, $tipo, $materia, $dificuldade, $resposta, $opcoes);
 
 	}
 }
