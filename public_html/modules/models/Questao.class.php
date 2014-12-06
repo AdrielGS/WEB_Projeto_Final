fbce8feada6294f2ffd2febc0b1f1c2936350a3f
ChicoLive <franciscodias@outlook.com>
2014-12-06 20:32:56 -0200

    PDO

--- public_html/modules/models/Questao.class.php
+++ public_html/modules/models/Questao.class.php
@@ -9,13 +9,22 @@ class Questao extends DB
 	function __construct(argument)
 	{}
 
-	public function add($enunciado, $tipo, $materia, $assunto, $dificuldade, $resposta, $opcoes){
-		foreach ($value as $opcoes)
-			$query_opcoes = DB::conn()->prepare("INSERT INTO __questoes_opcoes (question, value) VALUES ($question, $value)");
-
-		$query_questao = DB::conn()->prepare("INSERT INTO __questoes_questoes (enunciado, tipo, materia, assunto, dificuldade, resposta) VALUES ($enunciado, $tipo, $materia, $assunto, $dificuldade, $resposta, __questoes_opcoes)");
+public function add(){
+		ob_start();
+		include 'questoes.php';
+		$conteudo = ob_get_contents();
+		ob_end_clean();
+		$tamOpcoes = count($opcoes)
+		for ($cont=0; $cont < $tamOpcoes; $cont++) {
+			$value = $opcoes[$cont];
+			$correct = $resp[$cont]; 
+			$query_opcoes = DB::conn()->prepare("INSERT INTO __questoes_opcoes (question_id, value, correct) VALUES ($question_id, $value, $correct)");
+		}
+			
+		$query_questao = DB::conn()->prepare("INSERT INTO __questoes_questoes (value, type, difficulty, subject, tag) VALUES ($question, $type, $difficulty, $subject, $tag, __questoes_opcoes)");
 	}
 
 }
+
 
  ?>
